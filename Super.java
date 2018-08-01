7abe137486d8d70f27b2b35b074de188e8260ab7
Kartikey Tripathi <kartikey.tripathi.37@gmail.com>
2018-08-01 23:52:31 +0530

    Use of Super keyword

---
+++ Super.java
@@ -0,0 +1,40 @@
+class Room
+{
+	int length, breadth;
+	
+	Room(int x, int y)
+	{
+		length = x;
+		breadth = y;
+	}
+	int area()
+	{
+		return length * breadth;
+	}
+}
+class newRoom extends Room
+{
+	int height;
+	newRoom(int x, int y, int z)
+	{
+		super(x,y);
+		height = z;
+	}
+	int volume()
+	{
+		return length * breadth * height;
+	}
+}
+
+public class Super
+{
+	public static void main(String args[])
+	{
+		newRoom room1 = new newRoom(14,12,10);
+		int area1 = room1.area();
+		int volume1 = room1.volume();
+		System.out.println("Area is " + area1);
+		System.out.println("Volume is " + volume1);
+	}
+}
+	
